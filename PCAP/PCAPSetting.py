1444e387de051891aed32d05d92bf1bb3a318ae6
TONY <32955527+Metonydai@users.noreply.github.com>
2024-03-15 16:33:23 +0800

    Update PCAPSetting.py

--- PCAP/PCAPSetting.py
+++ PCAP/PCAPSetting.py
@@ -49,21 +49,27 @@ class Setting:
         # Remember user's last settings
         #self.widget.prefPCAPOutputFolder.setText(p.GetString("prefPCAPOutputFolder", ""))
         self.outputFolder = ""
+        #=====router=====
+        self.widget.rtDr.setText(p.GetString("prefPCAPDr", "3.0")) #shared_1
+        self.widget.rtAreaBound.setText(p.GetString("prefPCAPAreaBound", "500")) #shared_2
+        self.widget.rtDboard.setText(p.GetString("prefPCAPDboard", "0.15")) #shared_3
+        self.widget.rtHoleWidth.setText(p.GetString("rtHoleWidth", "6.0"))
+        self.widget.rtHoleAddLen.setText(p.GetString("rtHoleAddLen", "3.0"))
         #=====unloader=====
         self.widget.ulDr.setText(p.GetString("prefPCAPDr", "3.0")) #shared_1
-        self.widget.ulAreaBound.setText(p.GetString("prefPCAPAreaBound", "3.0")) #shared_2
-        self.widget.ulDboard.setText(p.GetString("prefPCAPDboard", "0.2"))
+        self.widget.ulAreaBound.setText(p.GetString("prefPCAPAreaBound", "500")) #shared_2
+        self.widget.ulDboard.setText(p.GetString("prefPCAPDboard", "0.2")) #shared_3
         self.widget.ulDl.setText(p.GetString("prefPCAPDl", "12.0"))
         self.widget.ulDw.setText(p.GetString("prefPCAPDw", "3.0"))
         #=====wave=====
         self.widget.prefPCAPDr.setText(p.GetString("prefPCAPDr", "3.0")) #shared_1
-        self.widget.prefPCAPAreaBound.setText(p.GetString("prefPCAPAreaBound", "3.0")) #shared_2
-        self.widget.prefPCAPDboard.setText(p.GetString("prefPCAPDboard", "0.2"))
+        self.widget.prefPCAPAreaBound.setText(p.GetString("prefPCAPAreaBound", "500")) #shared_2
+        self.widget.prefPCAPDboard.setText(p.GetString("prefPCAPDboard", "0.2")) #shared_3
         self.widget.prefPCAPDo.setText(p.GetString("prefPCAPDo", "1.0"))
         self.widget.prefPCAPDl.setText(p.GetString("prefPCAPDl", "12.0"))
         self.widget.prefPCAPDw.setText(p.GetString("prefPCAPDw", "3.0"))
         #=====press=====
-        self.widget.pressAreaBound.setText(p.GetString("prefPCAPAreaBound", "3.0")) #shared_2
+        self.widget.pressAreaBound.setText(p.GetString("prefPCAPAreaBound", "500")) #shared_2
         self.widget.pressDr.setText(p.GetString("prefPCAPDr", "3.0")) #shared_1
 
     def show(self):
@@ -79,7 +85,19 @@ class Setting:
     @QtCore.Slot()
     def on_saveButton_clicked(self):
         if self.widget.tabWidget.currentIndex() == 0:
-            pass
+            pcaplib.set_param("prefPCAPDr", self.widget.rtDr.text())
+            pcaplib.set_param("prefPCAPDboard", self.widget.rtDboard.text())
+            pcaplib.set_param("rtHoleWidth", self.widget.rtHoleWidth.text())
+            pcaplib.set_param("rtHoleAddLen", self.widget.rtHoleAddLen.text())
+            pcaplib.set_param("prefPCAPAreaBound", self.widget.rtAreaBound.text())
+
+            pcaplib.set_param("prefPCAPLayerOfBotsilk", self.widget.rtLayerOfBotsilk.currentText())
+            pcaplib.set_param("prefPCAPLayerOfBotpaste", self.widget.rtLayerOfBotpaste.currentText())
+            pcaplib.set_param("prefPCAPLayerOfBoardSink", self.widget.rtLayerOfBoardSink.currentText())
+            pcaplib.set_param("prefPCAPLayerOfOpen", self.widget.rtLayerOfOpen.currentText())
+            pcaplib.set_param("rtLayerOfRouterEdge", self.widget.rtLayerOfRouterEdge.currentText())
+            delimiter = ','
+            pcaplib.set_param("rtLayers", delimiter.join(self.get_item_text_from_selected_items(self.widget.rtListOfLayers)))
         elif self.widget.tabWidget.currentIndex() == 1:
             pcaplib.set_param("prefPCAPDr", self.widget.ulDr.text())
             pcaplib.set_param("prefPCAPDboard", self.widget.ulDboard.text())
@@ -130,18 +148,17 @@ class Setting:
     def on_playButton_clicked(self):
         self.widget.hide()
         if self.widget.tabWidget.currentIndex() == 0:
-            #ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('router'), FreeCAD.ActiveDocument)
-            print("I")
+            ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('router'), FreeCAD.ActiveDocument)
+            #LayerAnalysis.run_router()
         elif self.widget.tabWidget.currentIndex() == 1:
-            #ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('unloader'), FreeCAD.ActiveDocument)
-            print("I")
+            ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('unloader'), FreeCAD.ActiveDocument)
+            #LayerAnalysis.run_unloader()
         elif self.widget.tabWidget.currentIndex() == 2:
-            print("Love")
-            #ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('wave'), FreeCAD.ActiveDocument)
-            #LayerAnalysis.run_wave()
+            ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('wave'), FreeCAD.ActiveDocument)
+            LayerAnalysis.run_wave()
         elif self.widget.tabWidget.currentIndex() == 3:
-            #ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('press'), FreeCAD.ActiveDocument)
-            print("Huiyu")
+            ezlib.ezprocessdxf(self.dxfdoc, self.get_all_selected_layers('press'), FreeCAD.ActiveDocument)
+            #LayerAnalysis.run_press()
 
     @QtCore.Slot()
     def on_change_tab(self):
@@ -180,10 +197,11 @@ class Setting:
         obj_botpaste = []
         obj_botmask = []
         obj_open = []
-        obj_board_sink =[]
-        obj_fixed_support =[]
-        obj_stop_block =[]
-        obj_pressfit =[]
+        obj_board_sink = []
+        obj_fixed_support = []
+        obj_stop_block = []
+        obj_pressfit = []
+        obj_edge = []
         for i in range(len(objects)):
             if  re.search('bot', objects[i].lower()):
                 if re.search('silk', objects[i].lower()):
@@ -202,15 +220,25 @@ class Setting:
                 obj_stop_block.append(objects[i])
             elif re.search('press.*fit', objects[i].lower()):
                 obj_pressfit.append(objects[i])
+            elif re.search('edge', objects[i].lower()):
+                obj_edge.append(objects[i])
             else:
                 obj_array.append(objects[i])
         
+        # Add Layers For Unloader : tabWidget index = 0
+        self.widget.rtLayerOfBotsilk.addItems(obj_botsilk)
+        self.widget.rtLayerOfBotpaste.addItems(obj_botpaste)
+        self.widget.rtLayerOfBoardSink.addItems(obj_board_sink)
+        self.widget.rtLayerOfOpen.addItems(obj_open)
+        self.widget.rtLayerOfRouterEdge.addItems(obj_edge)
+        self.widget.rtListOfLayers.addItems(obj_array + obj_botmask + obj_fixed_support + obj_stop_block + obj_pressfit)
+
         # Add Layers For Unloader : tabWidget index = 1
         self.widget.ulLayerOfBotsilk.addItems(obj_botsilk)
         self.widget.ulLayerOfBotpaste.addItems(obj_botpaste)
         self.widget.ulLayerOfBotmask.addItems(obj_botmask)
         self.widget.ulLayerOfBoardSink.addItems(obj_board_sink)
-        self.widget.ulListOfLayers.addItems(obj_array + obj_open + obj_fixed_support + obj_stop_block + obj_pressfit)
+        self.widget.ulListOfLayers.addItems(obj_array + obj_open + obj_fixed_support + obj_stop_block + obj_pressfit + obj_edge)
 
         # Add Layers For Wave_Solder : tabWidget index = 2
         self.widget.prefPCAPLayerOfBotsilk.addItems(obj_botsilk)
@@ -218,7 +246,7 @@ class Setting:
         self.widget.prefPCAPLayerOfBotmask.addItems(obj_botmask)
         self.widget.prefPCAPLayerOfOpen.addItems(obj_open)
         self.widget.prefPCAPLayerOfBoardSink.addItems(obj_board_sink)
-        self.widget.prefPCAPListOfLayers.addItems(obj_array + obj_fixed_support + obj_stop_block + obj_pressfit)
+        self.widget.prefPCAPListOfLayers.addItems(obj_array + obj_fixed_support + obj_stop_block + obj_pressfit + obj_edge)
         
         # Add Layers For Press_Fit : tabWidget index = 3
         self.widget.pressLayerOfBotsilk.addItems(obj_botsilk)
@@ -226,7 +254,7 @@ class Setting:
         self.widget.pressLayerOfFixedSupport.addItems(obj_fixed_support)
         self.widget.pressLayerOfStopBlock.addItems(obj_stop_block)
         self.widget.pressLayerOfPressfit.addItems(obj_pressfit)
-        self.widget.pressListOfLayers.addItems(obj_array + obj_botmask +  obj_board_sink + obj_open)
+        self.widget.pressListOfLayers.addItems(obj_array + obj_botmask +  obj_board_sink + obj_open + obj_edge)
 
         # layers_open : for open switch swap use
         #self.layers_open = obj_open 
@@ -239,7 +267,15 @@ class Setting:
 
     
     def del_item_to_layer_selection(self):
-        # Delete Layers For Wave_Solder : tabWidget index = 1
+        # Delete Layers For Router : tabWidget index = 0
+        self.widget.rtLayerOfBotsilk.clear()
+        self.widget.rtLayerOfBotpaste.clear()
+        self.widget.rtLayerOfBoardSink.clear()
+        self.widget.rtLayerOfOpen.clear()
+        self.widget.rtLayerOfRouterEdge.clear()
+        self.widget.rtListOfLayers.clear()
+
+        # Delete Layers For Unloader tabWidget index = 1
         self.widget.ulLayerOfBotsilk.clear()
         self.widget.ulLayerOfBotpaste.clear()
         self.widget.ulLayerOfBotmask.clear()
@@ -272,7 +308,17 @@ class Setting:
     def get_all_selected_layers(self, case):
         l = []
         if case == "router":
-            pass
+            l.extend(self.get_item_text_from_selected_items(self.widget.rtListOfLayers))
+            if (self.widget.rtLayerOfBotsilk.currentText()):
+                l.append(self.widget.rtLayerOfBotsilk.currentText())
+            if (self.widget.rtLayerOfBotpaste.currentText()):
+                l.append(self.widget.rtLayerOfBotpaste.currentText())
+            if (self.widget.rtLayerOfBoardSink.currentText()):
+                l.append(self.widget.rtLayerOfBoardSink.currentText())
+            if (self.widget.rtLayerOfOpen.currentText()):
+                l.append(self.widget.rtLayerOfOpen.currentText())
+            if (self.widget.rtLayerOfRouterEdge.currentText()):
+                l.append(self.widget.rtLayerOfRouterEdge.currentText())
         elif case == "unloader":
             l.extend(self.get_item_text_from_selected_items(self.widget.ulListOfLayers))
             if (self.widget.ulLayerOfBotsilk.currentText()):
