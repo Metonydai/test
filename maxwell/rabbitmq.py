714fa8ca17f6bdbd3a188551b8c6339ad3f1c40e
TONY <32955527+Metonydai@users.noreply.github.com>
2024-09-26 23:29:12 +0800

    Update rabbitmq.py

--- maxwell/rabbitmq.py
+++ maxwell/rabbitmq.py
@@ -1,8 +1,11 @@
 from data_access.get_config import get_rmq_conn
+from data_access.logger import LoggerManager
+import os
 import pika
 import json
 from time import time
 import uuid
+import psutil
 
 # Need ACCOUNT, PWD, IP, PORT for MQ connection
 DEV_ACCOUNT, DEV_PWD, DEV_IP, DEV_PORT = get_rmq_conn("DEV")
@@ -11,6 +14,7 @@ PROD_ACCOUNT, PROD_PWD, PROD_IP, PROD_PORT = get_rmq_conn("PROD")
 CONSUMER_MESSAGE_QUEUE = "magnetic-simulation.request.simulate"  # CONSUMER QUEUE
 PRODUCER_EXCHANGE = "magenetic-simulation.response"  # PRODUCER EXCHANGE
 
+SIM_TIMEOUT = 7200
 
 class RabbitMQ():
     def __init__(self, queue_name="", exchange="", env="dev"):
@@ -18,12 +22,16 @@ class RabbitMQ():
         self.exchange = exchange
         if env == "dev":
             self.connection = self.connect(DEV_ACCOUNT, DEV_PWD, DEV_IP, DEV_PORT)
+            self.log = LoggerManager.getLog("dev")
         elif env == "stage":
             self.connection = self.connect(STAGE_ACCOUNT, STAGE_PWD, STAGE_IP, STAGE_PORT)
+            self.log = LoggerManager.getLog("stage")
         elif env == "prod":
             self.connection = self.connect(PROD_ACCOUNT, PROD_PWD, PROD_IP, PROD_PORT)
+            self.log = LoggerManager.getLog("prod")
         else:
             self.connection = self.connect(DEV_ACCOUNT, DEV_PWD, DEV_IP, DEV_PORT)
+            self.log = LoggerManager.getLog("dev")
 
         self.channel = self.connection.channel()
 
@@ -66,39 +74,75 @@ class RabbitMQ():
 
     def CONSUMER(self):
         def callback(ch, method, properties, body):
+            LOG = self.log
 
             # prepare response properties
             correlation_id = properties.correlation_id
             reply_to = properties.reply_to
 
+            env = properties.headers.get("SIS_environment")
+
             # ====== parse body to get request parameter =======
-            receive_dict = None
-            receive_dict = json.loads(body.decode())
-            # do something for request......
-            # ......
-            # ......
-            # ......
-            # ==================================================
+            #receive_dict = json.loads(body.decode())
+            serialized_dict = body.decode()
+            process = subprocess.Popen(["python", "maxwell.py", serialized_dict], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            sim_st_time = time.time()
+
+            #stdout, stderr = process.communicate() # Capture output
+            return_dict = {}
+            success_file = Path()
+            error_file = Path()
+
+            while True:
+                sim_time = time.time()
+                if (sim_time- sim_st_time) > SIM_TIMEOUT:
+                    log_msg = f"TIMEOUT Maxwell Simulation"
+                    LOG.info('(%s) %s', __name__, log_msg , extra={'correlationId': '['+ correlation_id +']'})
+                    print("TIMEOUT Maxwell Simulation", process.pid)
+                    os.system(TASKKILL_PID_FMT % str(process.pid))
+                    log_msg = f"TIMEOUT Kill Maxwell Simulation PID: {process.pid}"
+                    LOG.info('(%s) %s', __name__, log_msg , extra={'correlationId': '['+ correlation_id +']'})
+                    print(f"TIMEOUT Kill Maxwell Simulation PID: {process.pid}", process.pid)
+                    return_dict = {}
+                    break
+                
+                if (psutil.pid_exists(process.pid)):
+                    pid_state = psutil.Process(process.pid).status()
+                    if (pid_state == 'running'):
+                        log_msg = f"Normal Maxwell Simulation PID: {process.pid}"
+                        LOG.info('(%s) %s', __name__, log_msg , extra={'correlationId': '['+ correlation_id +']'})
+                        print("Normal Execution FLASK PID:", process.pid)
+                    else:
+                        os.system(TASKKILL_PID_FMT % str(process.pid))
+                        log_msg = f"Error Execution and Kill Maxwell Simulation PID: {process.pid}"
+                        LOG.error('(%s) %s', __name__, log_msg , extra={'correlationId': '['+ correlation_id+']'})
+                        print("Error Execution and Kill Maxwell Simulation PID:", process.pid)
+                        return_dict = {}
+                        break
+
+                if (success_file.exists()):
+                    return_dict = {}
+                    break
+                elif (error_file.exists()):
+                    return_dict = {}
+                    break
+
+                time.sleep(3)
 
             # ============ something write to queue ============
-            return_dict = {}
             username = properties.headers.get("username")
             message_id = uuid.uuid4()
             timestamp = int(time.time)
             service_name = 'electromagnetic_algorithm'
-            # make your return info......
-            # ......
-            # ......
-            # ......
-            # ==================================================
+
             response_mq = RabbitMQ(reply_to, PRODUCER_EXCHANGE)
             response_mq.producer(username, message_id, correlation_id,
                                  timestamp, service_name, json.dumps(return_dict))
             response_mq.close()
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
-            self.channel.cancel()
-            self.channel.close()
+            #self.channel.cancel()
+            #self.channel.close()
 
         method, properties, body = self.channel.basic_get(
             queue=self.queue_name, auto_ack=False)
